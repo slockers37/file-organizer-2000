b207925f3e43a2d43d6b3952193f2739edac79f2
slockers <slockers@pm.me>
2024-09-06 12:16:31 +0530

    feat: removed some endpoints

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -1,24 +1,8 @@
-import { anthropic } from "@ai-sdk/anthropic";
 import { createOpenAI } from '@ai-sdk/openai';
-
-const hecker = createOpenAI({
-  baseURL: 'https://api.heckerai.uk.to/v2',
-  apiKey: process.env.HECKER_API_KEY,
-});
 
 const groq = createOpenAI({
   baseURL: 'https://api.groq.com/openai/v1',
   apiKey: process.env.GROQ_API_KEY,
-});
-
-const hentai = createOpenAI({
-  baseURL: 'https://proxy.hentaigpt.xyz/v1',
-  apiKey: process.env.HENTAI_API_KEY,
-});
-
-const shadow = createOpenAI({
-  baseURL: 'https://shadowjourney.xyz/v1',
-  apiKey: process.env.SHADOW_API_KEY,
 });
 
 const zuki = createOpenAI({
@@ -26,25 +10,24 @@ const zuki = createOpenAI({
   apiKey: process.env.ZUKI_API_KEY,
 });
 
+const glhf = createOpenAI({
+  baseURL: 'https://api.zukijourney.com/v1',
+  apiKey: process.env.ZUKI_API_KEY,
+});
+
+
 
 const models = {
-  "hecker-gpt-4o": hecker("gpt-4o"),
-  "hecker-gpt-4o-2024-05-13": hecker("gpt-4o-2024-05-13", ),
   "zuki-gpt-4o": zuki("gpt-4o"),
-  "zuki-gpt-4o-2024-05-13": zuki("gpt-4o-2024-05-13", ),
-  "hentai-gpt-4o": hentai("gpt-4o"),
-  "hentai-gpt-4o-2024-05-13": hentai("gpt-4o-2024-05-13", ),
-  "hentai-gpt-4o-2024-08-06": hentai("gpt-4o-2024-08-06", ),
-  "shadow-gpt-4o": shadow("gpt-4o"),
-  "shadow-gpt-4o-2024-05-13": shadow("gpt-4o-2024-05-13", ),
   "groq-llama-3.1-70b": groq("llama-3.1-70b-versatile"),
+  "glhf-llama-3.1-405b": groq("hf:meta-llama/Meta-Llama-3.1-405B-Instruct"),
 };
 
 export const getModel = (name: string) => {
   if (!models[name]) {
     console.log(`Model ${name} not found`);
-    console.log(`Defaulting to hentai-gpt-4o`);
-    return models["hentai-gpt-4o"];
+    console.log(`Defaulting to groq-llama-3.1-70b`);
+    return models["groq-llama-3.1-70b"];
   }
   console.log(`Using model ${name}`);
   return models[name];

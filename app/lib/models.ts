7d87707e861ba549cabaae9b70a3b337651f4737
slockers <slockers@pm.me>
2024-09-06 09:48:27 +0530

    feat: removed some models and added others

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -21,21 +21,23 @@ const shadow = createOpenAI({
   apiKey: process.env.SHADOW_API_KEY,
 });
 
+const zuki = createOpenAI({
+  baseURL: 'https://api.zukijourney.com/v1',
+  apiKey: process.env.ZUKI_API_KEY,
+});
+
+
 const models = {
   "hecker-gpt-4o": hecker("gpt-4o"),
   "hecker-gpt-4o-2024-05-13": hecker("gpt-4o-2024-05-13", ),
-  "hecker-gpt-4o-mini": hecker("gpt-4o-mini"),
+  "zuki-gpt-4o": zuki("gpt-4o"),
+  "zuki-gpt-4o-2024-05-13": zuki("gpt-4o-2024-05-13", ),
   "hentai-gpt-4o": hentai("gpt-4o"),
   "hentai-gpt-4o-2024-05-13": hentai("gpt-4o-2024-05-13", ),
   "hentai-gpt-4o-2024-08-06": hentai("gpt-4o-2024-08-06", ),
-  "hentai-gpt-4o-mini": hentai("gpt-4o-mini"),
   "shadow-gpt-4o": shadow("gpt-4o"),
   "shadow-gpt-4o-2024-05-13": shadow("gpt-4o-2024-05-13", ),
-  "shadow-gpt-4o-mini": shadow("gpt-4o-mini"),
   "groq-llama-3.1-70b": groq("llama-3.1-70b-versatile"),
-  "groq-llama-3.1-8b": groq("llama-3.1-8b-instant"),
-  "groq-llama-3-70b": groq("llama3-70b-8192"),
-  "groq-llama-3-8b": groq("llama3-8b-8192"),
 };
 
 export const getModel = (name: string) => {

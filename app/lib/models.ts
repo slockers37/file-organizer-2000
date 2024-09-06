7731a87b8feb992be5341d75cf6805f5117327fd
slockers <slockers@pm.me>
2024-09-06 08:32:22 +0530

    feat: added more endpoints and models

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -1,18 +1,48 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from '@ai-sdk/openai';
+
+const hecker = createOpenAI({
+  baseURL: 'https://api.heckerai.uk.to/v2',
+  apiKey: process.env.HECKER_API_KEY,
+});
+
+const groq = createOpenAI({
+  baseURL: 'https://api.groq.com/openai/v1',
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+const hentai = createOpenAI({
+  baseURL: 'https://proxy.hentaigpt.xyz/v1',
+  apiKey: process.env.HENTAI_API_KEY,
+});
+
+const shadow = createOpenAI({
+  baseURL: 'https://shadowjourney.xyz/v1',
+  apiKey: process.env.SHADOW_API_KEY,
+});
 
 const models = {
-  "gpt-4o": openai("gpt-4o"),
-  "gpt-4o-2024-08-06": openai("gpt-4o-2024-08-06", ),
-  "gpt-4o-mini": openai("gpt-4o-mini"),
-  "claude-3-5-sonnet-20240620": anthropic("claude-3-5-sonnet-20240620"),
+  "hecker-gpt-4o": hecker("gpt-4o"),
+  "hecker-gpt-4o-2024-05-13": hecker("gpt-4o-2024-05-13", ),
+  "hecker-gpt-4o-mini": hecker("gpt-4o-mini"),
+  "hentai-gpt-4o": hentai("gpt-4o"),
+  "hentai-gpt-4o-2024-05-13": hentai("gpt-4o-2024-05-13", ),
+  "hentai-gpt-4o-2024-08-06": hentai("gpt-4o-2024-08-06", ),
+  "hentai-gpt-4o-mini": hentai("gpt-4o-mini"),
+  "shadow-gpt-4o": shadow("gpt-4o"),
+  "shadow-gpt-4o-2024-05-13": shadow("gpt-4o-2024-05-13", ),
+  "shadow-gpt-4o-mini": shadow("gpt-4o-mini"),
+  "groq-llama-3.1-70b": groq("llama-3.1-70b-versatile"),
+  "groq-llama-3.1-8b": groq("llama-3.1-8b-instant"),
+  "groq-llama-3-70b": groq("llama3-70b-8192"),
+  "groq-llama-3-8b": groq("llama3-8b-8192"),
 };
 
 export const getModel = (name: string) => {
   if (!models[name]) {
     console.log(`Model ${name} not found`);
-    console.log(`Defaulting to gpt-4o-2024-08-06`);
-    return models["gpt-4o-2024-08-06"];
+    console.log(`Defaulting to hentai-gpt-4o`);
+    return models["hentai-gpt-4o"];
   }
   console.log(`Using model ${name}`);
   return models[name];

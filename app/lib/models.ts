bab60e34a64ef9772ade90e5739f2a860aae2f17
slockers <slockers@pm.me>
2024-09-07 10:18:24 +0530

    removed glhf, added google

--- app/lib/models.ts
+++ app/lib/models.ts
@@ -1,4 +1,5 @@
 import { createOpenAI } from '@ai-sdk/openai';
+import { google } from '@ai-sdk/google';
 
 const groq = createOpenAI({
   baseURL: 'https://api.groq.com/openai/v1',
@@ -10,17 +11,11 @@ const zuki = createOpenAI({
   apiKey: process.env.ZUKI_API_KEY,
 });
 
-const glhf = createOpenAI({
-  baseURL: 'https://api.zukijourney.com/v1',
-  apiKey: process.env.ZUKI_API_KEY,
-});
-
-
-
 const models = {
   "zuki-gpt-4o": zuki("gpt-4o"),
   "groq-llama-3.1-70b": groq("llama-3.1-70b-versatile"),
-  "glhf-llama-3.1-405b": groq("hf:meta-llama/Meta-Llama-3.1-405B-Instruct"),
+  "gemini-pro-latest": google('gemini-1.5-pro-latest'),
+  "gemini-pro-exp": google('gemini-1.5-pro-exp-0827'),
 };
 
 export const getModel = (name: string) => {
